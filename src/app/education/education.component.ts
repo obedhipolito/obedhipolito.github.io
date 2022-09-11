ed707b1500c2992148ec98aee874fa07f2634b87
obed hipolito <obedhipolito@gmail.com>
2022-09-11 10:49:20 -0500

    cerrando education Close #9

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
+  education : Array<any> = [];
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    let educacion = {
+      fecha : "2019 -2022",
+      carrera : "Ingerienria de software",
+      universidad : "Universidad Veracruzana",
+    };
 
+    this.education.push(educacion)
+
+  }
 }
